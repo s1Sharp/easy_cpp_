423758f337eefd0be8b251d575b8f4846d6cab3a
Tsarkov Maksim <mtsarkov@naumen.ru>
2024-01-19 14:27:34 +0300

    init

---
+++ .vscode/cpp_prepare.sh
@@ -0,0 +1,6 @@
+sudo apt install build-essential cmake git
+git clone https://github.com/s1Sharp/easy_cpp_.git ~/cpp/project
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools-themes
+code ~/cpp/project
